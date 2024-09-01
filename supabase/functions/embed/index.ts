f6142fc8294f94b6322483d85f0d91cca2a36b24
Dan Jacobson <danrowejacobson@gmail.com>
2024-09-01 17:47:31 -0400

    feat: Add image processing pipeline for embedding generation

--- supabase/functions/embed/index.ts
+++ supabase/functions/embed/index.ts
@@ -1,6 +1,5 @@
 import { env, pipeline, RawImage } from 'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.17.2'
 import { createClient } from 'jsr:@supabase/supabase-js@2'
-import sharp from 'npm:sharp@0.32.6'
 
 // Because the tutorial said to
 env.allowLocalModels = false;
@@ -17,20 +16,6 @@ const pipe = await pipeline(
     'xenova/dinov2-small'
 )
 
-const loadImageFunction = async (/**@type {sharp.Sharp}*/img) => {
-    const metadata = await img.metadata();
-    const rawChannels = metadata.channels;
-
-    let { data, info } = await img.rotate().raw().toBuffer({ resolveWithObject: true });
-
-    const newImage = new RawImage(new Uint8ClampedArray(data), info.width, info.height, info.channels);
-    if (rawChannels !== undefined && rawChannels !== info.channels) {
-        // Make sure the new image has the same number of channels as the input image.
-        // This is necessary for grayscale images.
-        newImage.convert(rawChannels);
-    }
-    return newImage;
-}
 
 // Deno Handler
 Deno.serve(async (req) => {
@@ -46,15 +31,8 @@ Deno.serve(async (req) => {
         if (!imageFile || !(imageFile instanceof File)) {
             return new Response('No valid image file found in the request', { status: 400 });
         }
-
-        // Convert the File to a Buffer
-        const imageBuffer = await imageFile.arrayBuffer();
-
-        // Use sharp to process the image
-        const sharpImage = sharp(imageBuffer);
-
-        // Create a RawImage using the new loadImageFunction
-        const img = await loadImageFunction(sharpImage);
+        // Parse image file here
+        // img = RawImage(Uin8Array, width, height, channels);
         
         // Generate the embedding
         const output = await pipe(img);
