debd69324176fe5e58cd0eff017c4d02da6a4c70
Dan Jacobson <danrowejacobson@gmail.com>
2024-09-28 21:28:23 -0400

    feat: Add XGBoostModel to model_classes

--- src/doggelganger/models/resnet.py
+++ src/doggelganger/models/resnet.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 import optuna
 
+
 class ResidualBlock(nn.Module):
     def __init__(self, dim):
         super(ResidualBlock, self).__init__()
@@ -16,10 +17,13 @@ class ResidualBlock(nn.Module):
         residual = self.relu(self.fc1(x))
         return x + self.fc2(residual)
 
+
 class MinimalPerturbationNetwork(nn.Module):
     def __init__(self, embedding_dim, num_blocks=3):
         super(MinimalPerturbationNetwork, self).__init__()
-        self.blocks = nn.ModuleList([ResidualBlock(embedding_dim) for _ in range(num_blocks)])
+        self.blocks = nn.ModuleList(
+            [ResidualBlock(embedding_dim) for _ in range(num_blocks)]
+        )
         self.init_weights()
 
     def forward(self, x):
@@ -29,16 +33,23 @@ class MinimalPerturbationNetwork(nn.Module):
 
     def init_weights(self):
         for name, param in self.named_parameters():
-            if 'weight' in name:
+            if "weight" in name:
                 nn.init.normal_(param, mean=0, std=1e-3)  # Very small initial weights
-            elif 'bias' in name:
+            elif "bias" in name:
                 nn.init.constant_(param, 0)
 
+
 class ResNetModel(BaseModel):
-    def __init__(self, num_blocks=3, learning_rate=0.001, lambda_delta=0.1, lambda_ortho=0.1):
+    def __init__(
+        self, num_blocks=3, learning_rate=0.001, lambda_delta=0.1, lambda_ortho=0.1
+    ):
         self.num_blocks = num_blocks
-        self.model = MinimalPerturbationNetwork(384, num_blocks)  # Assuming DinoV2 embedding size
-        self.device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+        self.model = MinimalPerturbationNetwork(
+            384, num_blocks
+        )  # Assuming DinoV2 embedding size
+        self.device = torch.device(
+            "mps" if torch.backends.mps.is_available() else "cpu"
+        )
         self.model.to(self.device)
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
         self.criterion = nn.MSELoss()
@@ -54,50 +65,58 @@ class ResNetModel(BaseModel):
         for epoch in tqdm(range(num_epochs)):
             epoch_loss = 0
             for i in range(0, len(X), batch_size):
-                batch_X = X[i:i+batch_size]
-                batch_y = y[i:i+batch_size]
+                batch_X = X[i : i + batch_size]
+                batch_y = y[i : i + batch_size]
 
                 self.optimizer.zero_grad()
                 outputs = self.model(batch_X)
-                
+
                 loss_main = self.criterion(outputs, batch_y)
                 loss_delta = torch.norm(outputs - batch_X, p=2)
                 loss_ortho = 0
                 for block in self.model.blocks:
                     loss_ortho += torch.norm(
-                        torch.mm(block.fc1.weight, block.fc1.weight.t()) - torch.eye(384).to(self.device)
+                        torch.mm(block.fc1.weight, block.fc1.weight.t())
+                        - torch.eye(384).to(self.device)
                     ) + torch.norm(
-                        torch.mm(block.fc2.weight, block.fc2.weight.t()) - torch.eye(384).to(self.device)
+                        torch.mm(block.fc2.weight, block.fc2.weight.t())
+                        - torch.eye(384).to(self.device)
                     )
-                
-                loss = loss_main + self.lambda_delta * loss_delta + self.lambda_ortho * loss_ortho
-                
+
+                loss = (
+                    loss_main
+                    + self.lambda_delta * loss_delta
+                    + self.lambda_ortho * loss_ortho
+                )
+
                 loss.backward()
                 self.optimizer.step()
 
                 # Log metrics
-                self.writer.add_scalar('Loss/total', loss.item(), global_step)
-                self.writer.add_scalar('Loss/main', loss_main.item(), global_step)
-                self.writer.add_scalar('Loss/delta', loss_delta.item(), global_step)
-                self.writer.add_scalar('Loss/ortho', loss_ortho.item(), global_step)
+                self.writer.add_scalar("Loss/total", loss.item(), global_step)
+                self.writer.add_scalar("Loss/main", loss_main.item(), global_step)
+                self.writer.add_scalar("Loss/delta", loss_delta.item(), global_step)
+                self.writer.add_scalar("Loss/ortho", loss_ortho.item(), global_step)
 
                 epoch_loss += loss.item()
                 global_step += 1
 
             # Log epoch average loss
-            self.writer.add_scalar('Loss/epoch', epoch_loss / (len(X) // batch_size), epoch)
+            self.writer.add_scalar(
+                "Loss/epoch", epoch_loss / (len(X) // batch_size), epoch
+            )
 
         self.writer.close()
         return epoch_loss / (len(X) // batch_size)
 
     @staticmethod
     def objective(trial, X, y):
-        num_blocks = trial.suggest_int('num_blocks', 2, 5)
-        learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-2)
-        lambda_delta = trial.suggest_loguniform('lambda_delta', 1e-3, 1e-1)
-        lambda_ortho = trial.suggest_loguniform('lambda_ortho', 1e-3, 1e-1)
-        num_epochs = trial.suggest_int('num_epochs', 50, 200)
-        batch_size = trial.suggest_categorical('batch_size', [16, 32, 64])
+        num_blocks = trial.suggest_int("num_blocks", 2, 5)
+        learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True)
+        lambda_delta = trial.suggest_float("lambda_delta", 1e-3, 1e-1, log=True)
+        lambda_ortho = trial.suggest_float("lambda_ortho", 1e-3, 1e-1, log=True)
+        num_epochs = trial.suggest_int("num_epochs", 50, 200)
+        batch_size = trial.suggest_categorical("batch_size", [16, 32, 64])
 
         model = ResNetModel(num_blocks, learning_rate, lambda_delta, lambda_ortho)
         final_loss = model.fit(X, y, num_epochs, batch_size)
@@ -105,17 +124,22 @@ class ResNetModel(BaseModel):
 
     @classmethod
     def hyperparameter_search(cls, X, y, n_trials=100):
-        study = optuna.create_study(direction='minimize')
+        study = optuna.create_study(direction="minimize")
         study.optimize(lambda trial: cls.objective(trial, X, y), n_trials=n_trials)
 
         best_params = study.best_params
         best_model = cls(
-            num_blocks=best_params['num_blocks'],
-            learning_rate=best_params['learning_rate'],
-            lambda_delta=best_params['lambda_delta'],
-            lambda_ortho=best_params['lambda_ortho']
+            num_blocks=best_params["num_blocks"],
+            learning_rate=best_params["learning_rate"],
+            lambda_delta=best_params["lambda_delta"],
+            lambda_ortho=best_params["lambda_ortho"],
         )
-        best_model.fit(X, y, num_epochs=best_params['num_epochs'], batch_size=best_params['batch_size'])
+        best_model.fit(
+            X,
+            y,
+            num_epochs=best_params["num_epochs"],
+            batch_size=best_params["batch_size"],
+        )
         return best_model, best_params
 
     def predict(self, X):
--- src/doggelganger/train.py
+++ src/doggelganger/train.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from tqdm import tqdm
 
 from doggelganger.utils import get_embedding, load_model as load_embedding_model
-from doggelganger.models import LinearRegressionModel, ResNetModel
+from doggelganger.models import LinearRegressionModel, XGBoostModel, ResNetModel
 
 # Set up logging
 logging.basicConfig(
@@ -201,7 +201,7 @@ def main():
     model_classes = {
         "linear": LinearRegressionModel,
         "xgboost": XGBoostModel,
-        "resnet": ResNetModel
+        "resnet": ResNetModel,
     }
 
     model_class = model_classes[args.model]
