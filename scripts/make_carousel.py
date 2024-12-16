f5fcfba00a3f93bfab6ad2633937ef083f0351b5
Dan Jacobson <danrowejacobson@gmail.com>
2024-12-16 12:49:14 -0500

    feat: Increase default number of images in carousel from 10 to 20

--- scripts/make_carousel.py
+++ scripts/make_carousel.py
@@ -25,7 +25,7 @@ def create_safe_filename(entry):
 def parse_args():
     parser = argparse.ArgumentParser(description='Create a carousel of dog images')
     parser.add_argument('--file', type=str, required=True, help='Input JSONL file')
-    parser.add_argument('--N', type=int, default=10, help='Number of images to select')
+    parser.add_argument('--N', type=int, default=20, help='Number of images to select')
     return parser.parse_args()
 
 def read_jsonl(file_path):
